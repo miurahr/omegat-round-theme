39f0e284c57e7940272187fca73b5d6f90a6d646
Hiroshi Miura <miurahr@linux.com>
2022-03-15 20:38:04 +0900

    Gradle: use git version for versioning
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,17 +4,40 @@ plugins {
     distribution
     maven
     id("org.omegat.gradle") version "1.5.7"
+    id("com.palantir.git-version") version "0.12.3" apply false
 }
 
-version = "0.0.1"
+// we handle cases without .git directory
+val dotgit = project.file(".git")
+if (dotgit.exists()) {
+    // calculate version string from git tag, hash and commit distance
+    apply(plugin = "com.palantir.git-version")
+    val versionDetails: groovy.lang.Closure<com.palantir.gradle.gitversion.VersionDetails> by extra
+    val details = versionDetails()
+    val baseVersion = details.lastTag.substring(1)
+    if (details.isCleanTag) {  // release version
+        version = baseVersion
+    } else {  // snapshot version
+        version = baseVersion + "-" + details.commitDistance + "-" + details.gitHash + "-SNAPSHOT"
+    }
+} else {
+    println("Read version property from gradle.properties.")
+}
 
-repositories {
-    mavenCentral()
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+    withSourcesJar()
+    withJavadocJar()
 }
 
 omegat {
     version = "5.6.0"
     pluginClass = "tokyo.northside.omegat.theme.RoundThemes"
+}
+
+repositories {
+    mavenCentral()
 }
 
 dependencies {
@@ -24,6 +47,11 @@ dependencies {
 checkstyle {
     isIgnoreFailures = true
     toolVersion = "7.1"
+}
+
+tasks.withType<JavaCompile> {
+    options.compilerArgs.add("-Xlint:deprecation")
+    options.compilerArgs.add("-Xlint:unchecked")
 }
 
 distributions {
